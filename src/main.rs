2c6268f0a233136ae1a050d6a44e92ec202af5e9
emanon001 <emanon001@gmail.com>
2021-09-09 22:35:42 +0900

    feat: add `Expr`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Expr {
+    Number(i64),
+    Add(Box<Expr>, Box<Expr>),
+    Multiply(Box<Expr>, Box<Expr>),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let expr = Expr::Add(
+        Box::new(Expr::Multiply(
+            Box::new(Expr::Number(1)),
+            Box::new(Expr::Number(2)),
+        )),
+        Box::new(Expr::Multiply(
+            Box::new(Expr::Number(3)),
+            Box::new(Expr::Number(4)),
+        )),
+    );
+    println!("{:?}", expr);
 }

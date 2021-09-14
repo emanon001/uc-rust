26a4daab97163583fcfe3f3170b9f2528d886553
emanon001 <emanon001@gmail.com>
2021-09-14 23:26:46 +0900

    feat: `Stmt::Assign`を追加

--- src/main.rs
+++ src/main.rs
@@ -92,12 +92,29 @@ impl fmt::Debug for Expr {
 #[derive(PartialEq, Eq, Clone)]
 enum Stmt {
     DoNothing,
+    Assign(String, Expr),
 }
 
 impl Stmt {
     fn is_reducible(&self) -> bool {
         match self {
             Self::DoNothing => false,
+            Self::Assign(_, _) => true,
+        }
+    }
+
+    fn reduce(&self, env: &Environment) -> (Stmt, Environment) {
+        match self {
+            Self::Assign(name, expr) => {
+                if expr.is_reducible() {
+                    (Self::Assign(name.into(), expr.reduce(env)), env.clone())
+                } else {
+                    let mut new_env = env.clone();
+                    new_env.insert(name.into(), expr.clone());
+                    (Self::DoNothing, new_env)
+                }
+            }
+            _ => panic!("`reduce()` not supported"),
         }
     }
 }
@@ -106,6 +123,7 @@ impl fmt::Display for Stmt {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         match self {
             Self::DoNothing => write!(f, "do-nothing"),
+            Self::Assign(name, val) => write!(f, "{} = {}", name, val),
         }
     }
 }
@@ -117,36 +135,40 @@ impl fmt::Debug for Stmt {
 }
 
 struct Machine {
-    expr: Expr,
+    stmt: Stmt,
     env: Environment,
 }
 
 impl Machine {
-    fn new(expr: Expr, env: Environment) -> Self {
-        Self { expr, env }
+    fn new(stmt: Stmt, env: Environment) -> Self {
+        Self { stmt, env }
     }
 
     fn step(&mut self) {
-        self.expr = self.expr.reduce(&self.env);
+        let (new_stmt, new_env) = self.stmt.reduce(&self.env);
+        self.stmt = new_stmt;
+        self.env = new_env;
     }
 
     fn run(&mut self) {
-        while self.expr.is_reducible() {
-            println!("{}", self.expr);
+        while self.stmt.is_reducible() {
+            println!("{}, {:?}", self.stmt, self.env);
             self.step();
         }
-        println!("{}", self.expr);
+        println!("{}, {:?}", self.stmt, self.env);
     }
 }
 
 fn main() {
-    let expr = Expr::Add(
-        Box::new(Expr::Variable("x".into())),
-        Box::new(Expr::Variable("y".into())),
+    let stmt = Stmt::Assign(
+        "x".into(),
+        Expr::Add(
+            Box::new(Expr::Variable("x".into())),
+            Box::new(Expr::Number(1)),
+        ),
     );
     let mut env = HashMap::new();
-    env.insert("x".to_string(), Expr::Number(3));
-    env.insert("y".to_string(), Expr::Number(4));
-    let mut machine = Machine::new(expr, env);
+    env.insert("x".to_string(), Expr::Number(2));
+    let mut machine = Machine::new(stmt, env);
     machine.run();
 }
